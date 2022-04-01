7183c9478ba4e9dbd05692035e86f2df392ef68b
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-01 18:27:04 +0300

    Create associations and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'home#index'
+  resources :books do
+    member do
+      put "add", to: "books#library"
+      put "remove", to: "books#library"
+    end
+  end
+  resources :library, only: [:index]
+  resources :pricing, only: [:index]
+  devise_for :users, controllers: { registrations: "registrations" }
+  root to: 'books#index'
+  resources :subscriptions, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_01_110939) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_01_121027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,15 +42,44 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_110939) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_id"
+    t.string "stripe_subscription_id"
+    t.string "card_last4"
+    t.integer "card_exp_month"
+    t.integer "card_exp_year"
+    t.string "card_type"
+    t.boolean "admin"
+    t.boolean "subscribed"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
